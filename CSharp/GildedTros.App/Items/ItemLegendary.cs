6257feb5ad427439b4c8f338083251ce6ece5a02
JanVdVeken <vekie>
2022-01-06 16:28:23 +0100

    Initial Legendary Item setup

---
+++ CSharp/GildedTros.App/Items/ItemLegendary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GildedTros.App.Items
+{
+    public class ItemLegendary : Item, IAgingItem
+    {
+        public int QualitySpeed { get; }
+        public int SellInSpeed { get; }
+
+        public ItemLegendary(string name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException();
+            Name = name;
+            QualitySpeed = +0;
+            SellInSpeed = -0;
+        }
+        public void Age()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CSharp/GildedTros.Test/Unit/ItemLegendaryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GildedTros.App.Items;
+using GildedTros.Test.Unit.Helper;
+using Xunit;
+
+namespace GildedTros.Test.Unit
+{
+    public class ItemLegendaryTest
+    {
+        [Fact]
+        public void Ctor_GivenNoName_ShouldReturnNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ItemLegendary(null));
+        }
+        [Fact]
+        public void Ctor_GivenSomeName_ShouldHaveThatName()
+        {
+            var itemLegendary = new ItemLegendary(TestItems.SomeName);
+            
+            Assert.Equal(TestItems.SomeName,itemLegendary.Name);
+        }
+    }
+}
+
